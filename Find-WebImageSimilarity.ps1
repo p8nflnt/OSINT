2b1904eed4e5e3b98376a380990dfca0aa9a2bd9
Payton Flint <47405653+p8nflnt@users.noreply.github.com>
2024-10-26 11:44:01 -0500

    Create Find-WebImageSimilarity.ps1

---
+++ Find-WebImageSimilarity.ps1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+    To locate similar photos on a web site
+      1. Provide a base image
+      2. Generate a perceptive hash
+      3. Crawl a particular site
+      4. Generate perceptive hashes for all found images
+      5. Compare the perceptive hashes
+
+.NOTES
+    Name: Find-WebImageSimilarity.ps1
+    Author: Payton Flint
+    Version: 1.0
+    DateCreated: 2024-Oct
+
+.LINK
+    https://www.joshooaj.com/blog/2024/03/24/image-comparison/
+    https://youtu.be/TTRaDlRWnGg?si=1MSGlU7Ey9HruU1q
+    
+#>
+
